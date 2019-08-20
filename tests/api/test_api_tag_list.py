c0dc2541c7900446579c9d731d34d0b3946afdd4
cle-b <cle@tictac.pm>
2019-08-20 13:45:29 +0200

    typo

--- tests/api/test_api_tag_list.py
+++ tests/api/test_api_tag_list.py
@@ -7,7 +7,7 @@ import pytest
 @pytest.mark.vcr()
 def test_api_tag_list_anonymous(api, new_task_done_three_tags):
     """
-    1. List all tags. The tags of a newly created tag is present.
+    1. List all tags. The tags of a newly created task is present.
     """
     tags = api.list_tags()
     for tag in new_task_done_three_tags.tags:
@@ -20,7 +20,7 @@ def test_api_tag_list_anonymous(api, new_task_done_three_tags):
 def test_api_tag_list_with_user(api, new_user, new_task_done_three_tags):
     """
     1. Sign in.
-    1. List all tags. The tags of a newly created tag is present.
+    1. List all tags. The tags of a newly created task is present.
     """
     api.authenticate(new_user.username, new_user.password)
     tags = api.list_tags()
